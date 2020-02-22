b3056b3921dcb6ffe6b48c2ba1a1cb108313d3be
Octávio Paulo <octavio.j.p.paulo@gmail.com>
2020-02-22 22:11:09 +0000

    Commit section 04 video 17

--- src/main/java/com/carros/api/IndexController.java
+++ src/main/java/com/carros/api/IndexController.java
@@ -1,10 +1,8 @@
 package com.carros.api;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,18 +14,8 @@ public class IndexController {
 		return "Get Spring Boot!";
 	}
 
-	@PostMapping
-	public String post() {
-		return "Post Spring Boot!";
-	}
-
-	@PutMapping
-	public String put() {
-		return "Put Spring Boot!";
-	}
-
-	@DeleteMapping
-	public String delete() {
-		return "Delete Spring Boot!";
+	@GetMapping("/login")
+	public String get(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+		return "Login: " + login + ", senha: " + senha;
 	}
 }
