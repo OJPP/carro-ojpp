66203e9e40365a19d8649c8226932ad6e81e9e88
Octávio Paulo <octavio.j.p.paulo@gmail.com>
2020-02-22 22:54:16 +0000

    Commit section 05 video 20

--- src/main/java/com/carros/api/IndexController.java
+++ src/main/java/com/carros/api/IndexController.java
@@ -1,9 +1,7 @@
 package com.carros.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,12 +10,6 @@ public class IndexController {
 
 	@GetMapping
 	public String get() {
-		return "Get Spring Boot!";
+		return "API dos carros!";
 	}
-
-	@PostMapping("/login")
-	public String get(@RequestParam("login") String login, @RequestParam("senha") String senha) {
-		return "Login: " + login + ", senha: " + senha;
-	}
-
 }
