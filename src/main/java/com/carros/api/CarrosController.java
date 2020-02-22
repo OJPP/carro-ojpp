0cbe4eb302cfe0aa8af8d129e806c361a87cb169
Octávio Paulo <octavio.j.p.paulo@gmail.com>
2020-02-22 23:36:11 +0000

    Commit section 05 video 23

--- src/main/java/com/carros/api/CarrosController.java
+++ src/main/java/com/carros/api/CarrosController.java
@@ -2,6 +2,7 @@ package com.carros.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,8 @@ import com.carros.domain.CarroService;
 @RequestMapping("/api/v1/carros")
 public class CarrosController {
 
-	private CarroService service = new CarroService();
+	@Autowired
+	private CarroService service;
 
 	@GetMapping
 	public List<Carro> get() {
