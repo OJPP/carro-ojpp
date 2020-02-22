122a94da42032397f3f55c13c181f0a27a88fec1
Octávio Paulo <octavio.j.p.paulo@gmail.com>
2020-02-22 22:28:07 +0000

    Commit section 04 video 18

--- src/main/java/com/carros/api/IndexController.java
+++ src/main/java/com/carros/api/IndexController.java
@@ -1,8 +1,8 @@
 package com.carros.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +14,18 @@ public class IndexController {
 		return "Get Spring Boot!";
 	}
 
-	@GetMapping("/login")
-	public String get(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+	@GetMapping("/login/{login}/senha/{senha}")
+	public String get(@PathVariable("login") String login, @PathVariable("senha") String senha) {
 		return "Login: " + login + ", senha: " + senha;
+	}
+
+	@GetMapping("/carro/{id}")
+	public String getCarroById(@PathVariable("id") Long id) {
+		return "Carro by id " + id;
+	}
+
+	@GetMapping("/carro/tipo/{tipo}")
+	public String getCarroByTipo(@PathVariable("tipo") String tipo) {
+		return "Lista de Carros " + tipo;
 	}
 }
