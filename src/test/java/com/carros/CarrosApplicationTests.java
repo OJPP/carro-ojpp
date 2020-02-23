18c692433b6e1380f959fd046699fd2299d3435a
Octávio Paulo <octavio.j.p.paulo@gmail.com>
2020-02-23 16:51:06 +0000

    Commit section 09 video 44

--- src/test/java/com/carros/CarrosApplicationTests.java
+++ src/test/java/com/carros/CarrosApplicationTests.java
@@ -1,13 +1,26 @@
 package com.carros;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.carros.domain.Carro;
+import com.carros.domain.CarroService;
 
 @SpringBootTest
 class CarrosApplicationTests {
 
+	@Autowired
+	private CarroService carroService;
+
 	@Test
 	void teste1() {
+
+		Carro carro = new Carro();
+		carro.setNome("Ferrari");
+		carro.setTipo("esportivo");
+		
+		carroService.insert(carro);
 	}
 
 	@Test
