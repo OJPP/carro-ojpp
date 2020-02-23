36ec433e8b972993408ca1354e008511c5f0ae9b
Octávio Paulo <octavio.j.p.paulo@gmail.com>
2020-02-23 10:45:28 +0000

    Commit section 05 video 29

--- src/main/java/com/carros/domain/CarroService.java
+++ src/main/java/com/carros/domain/CarroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class CarroService {
@@ -25,8 +26,46 @@ public class CarroService {
 		return rep.findByTipo(tipo);
 	}
 
-	public Carro save(Carro carro) {
+	public Carro insert(Carro carro) {
 		return rep.save(carro);
+	}
+
+	public Carro update(Carro carro, Long id) {
+
+		Assert.notNull(id, "Não foi possivel actualizar o registo");
+
+//		// Busca o carro na base de dados
+//        Optional<Carro> carroOptional = rep.findById(id);
+//
+//		if(carroOptional.isPresent()) {
+//			Carro carroDB = carroOptional.get();
+//
+//			// Copiar as propriedades
+//			carroDB.setNome(carro.getNome());
+//			carroDB.setTipo(carro.getTipo());
+//			System.out.println("Carro id " + carroDB.getId());
+//			
+//			//Actualiza o carro
+//			rep.save(carroDB);
+//
+//			return carroDB;
+//		} else {
+//			throw new RuntimeException("Não foi possivel actualizar o registo");
+//		}
+
+		this.getCarroById(id).map( carroDB -> {
+			// Copiar as propriedades
+			carroDB.setNome(carro.getNome());
+			carroDB.setTipo(carro.getTipo());
+			System.out.println("Carro id " + carroDB.getId());
+			
+			//Actualiza o carro
+			rep.save(carroDB);
+
+			return carroDB;
+		}).orElseThrow( () -> new RuntimeException("Não foi possivel actualizar o registo"));
+
+		return null;
 	}
 
 	public List<Carro> getCarrosFake() {
