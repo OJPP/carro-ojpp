9183009fc7d7eca3576932b5fa48d16e3fd78566
Octávio Paulo <octavio.j.p.paulo@gmail.com>
2020-02-23 17:26:31 +0000

    Commit section 09 video 45

--- src/test/java/com/carros/CarrosApplicationTests.java
+++ src/test/java/com/carros/CarrosApplicationTests.java
@@ -1,11 +1,19 @@
 package com.carros;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.carros.domain.Carro;
 import com.carros.domain.CarroService;
+import com.carros.domain.dto.CarroDTO;
 
 @SpringBootTest
 class CarrosApplicationTests {
@@ -20,7 +28,25 @@ class CarrosApplicationTests {
 		carro.setNome("Ferrari");
 		carro.setTipo("esportivo");
 		
-		carroService.insert(carro);
+		CarroDTO carroDTO = carroService.insert(carro);
+		assertNotNull(carroDTO);
+		
+		Long id = carroDTO.getId();
+		assertNotNull(id);
+
+		// Ler o carro da base de dados
+		Optional<CarroDTO> carroDTOBd = carroService.getCarroById(id);
+		assertTrue(carroDTOBd.isPresent());
+		
+		carroDTO = carroDTOBd.get();
+		assertEquals("Ferrari", carroDTO.getNome());
+		assertEquals("esportivo", carroDTO.getTipo());
+		
+		// Eliminar o carro da base de dados
+		carroService.delete(id);
+
+		// Verificar se o carro foi eliminado da base de dados
+		assertFalse(carroService.getCarroById(id).isPresent());
 	}
 
 	@Test
