b8ff82df26fcb78e0f256bff6aeb42fad2ec1c3d
Octávio Paulo <octavio.j.p.paulo@gmail.com>
2020-02-25 11:04:51 +0000

    Commit section 12 video 63

--- src/main/java/com/carros/api/security/UserDetailsServiceImpl.java
+++ src/main/java/com/carros/api/security/UserDetailsServiceImpl.java
@@ -1,27 +1,31 @@
 package com.carros.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+//import com.carros.domain.User;
+import com.carros.domain.UserRepository;
 
 @Service(value = "userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-		if(username.equals("user")) {
-			return User.withUsername("user").password(encoder.encode("user")).roles("USER").build();
-		} else if(username.equals("admin")) {
-			return User.withUsername("admin").password(encoder.encode("admin")).roles("USER","ADMIN").build();
+		com.carros.domain.User user = userRepository.findByLogin(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("User não encontrado");
 		}
+
+		return User.withUsername(username).password(user.getSenha()).roles("USER").build();
 		
-		throw new UsernameNotFoundException("User não encontrado");
 	}
 
 }
---
+++ src/main/java/com/carros/domain/User.java
@@ -0,0 +1,31 @@
+package com.carros.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String nome;
+	private String login;
+	private String senha;
+	private String email;
+
+	public static void main(String[] args) {
+
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+		System.out.println(encoder.encode("albatroz"));
+	}
+
+}
